f4a1340a95b0663c911bb7c4b668b230c1faf4bd
SAI JAGANNADH <68389115+SaiSatyaJagannadh@users.noreply.github.com>
2026-06-13 21:40:35 -0400

    basic file change and installed ollama in local

--- main_ollama.py
+++ main_ollama.py
@@ -3,7 +3,7 @@ from langchain.chat_models import init_chat_model
 
 model = init_chat_model(
     model='llama3.2:1b',
-    model_provider="ollama"
+    model_provider="ollama" #this running on our laptop  by 'ollama pull llama3.2:1b' in terminal
 )
 
 response = model.invoke("Can I learn Python in one year?")
